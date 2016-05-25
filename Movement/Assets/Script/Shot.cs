bab3e3fba89b9632bbd17316155c97db36cd3632
Emiilly <emilytimmerman@hotmail.co.uk>
2016-05-25 12:43:57 +0200

    Game

---
+++ Movement/Assets/Script/Shot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+
+    public float speed = 3.0f;
+    private Rigidbody2D rigi;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+        rigi.velocity = new Vector2(0.0f, speed);
+
+    }
+    // Function called when the object goes out of the screen
+   void OnBecameInvisible()
+    {
+        // Destroy the bullet 
+        Destroy(gameObject);
+    }
+}
---
+++ Movement/Assets/Script/SpaceShipmovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShipmovement : MonoBehaviour {
+   public GameObject furball2;
+    public float speed = 10.0f;
+    public float minX = -5.0f;
+    public float maxX = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.position += new Vector3(delta, 0.0f);
+
+        if (transform.position.x < minX)
+        {
+            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        }
+        // When the spacebar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create a new bullet at “transform.position” 
+            // Which is the current position of the ship
+            // Quaternion.identity = add the bullet with no rotation
+            Instantiate(furball2, transform.position, Quaternion.identity);
+        }
+
+    }
+}
