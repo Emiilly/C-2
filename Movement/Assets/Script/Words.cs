fb071b4a7249b27000912a7328d3f3e19a8714dc
anne818x <annemarieke-m@hotmail.com>
2016-05-27 12:03:26 +0200

    spell a word

---
+++ Movement/Assets/Script/Words.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script
+{
+    public class Words
+    {
+        static string printedWord = "BALL";
+
+        public Words()
+        {
+
+        }
+
+        public string getNext(int next)
+        {
+            return printedWord[next].ToString();
+        }
+
+        
+    }
+}
