bad47d6b8e1512aa5a48c1c9edf0361d67481b94
Emiilly <emilytimmerman@hotmail.co.uk>
2016-05-27 09:36:37 +0200

    Collision to a bee
    working progress

---
+++ Movement/Assets/Script/DamageHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageHandler : MonoBehaviour {
+
+    int health = 1;
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("Trigger!");
+        health--;
+        if (health<= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
