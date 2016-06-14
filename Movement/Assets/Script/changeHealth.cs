ee7a1210f95820d8ac836b3eeb19a0f60e6480bf
Emiilly <emilytimmerman@hotmail.co.uk>
2016-06-14 16:30:45 +0200

    Timer on images and comments

--- Movement/Assets/Script/changeHealth.cs
+++ Movement/Assets/Script/changeHealth.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class changeHealth : MonoBehaviour {
+    /// <summary>
+    /// Variables containing the sprites to change the health
+    /// they are set in the unity GUI
+    /// </summary>
     public Sprite fullHp;
     public Sprite twoHp;
     public Sprite oneHp;
@@ -11,17 +15,22 @@ public class changeHealth : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        sprite = GetComponent<Image>();
+        sprite = GetComponent<Image>(); //refer to the Image box on the field
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        CheckHealth();
+        CheckHealth();//call check health method every frame
     }
 
+    /// <summary>
+    /// Method that checks the lives variable from Bubbles class and shows 
+    /// different sprite dependng on the lives
+    /// </summary>
     public void CheckHealth()
     {
+        //get value for lives
         int lives = Bubbles.getNumberLives();
         if (lives == 3)
         {
@@ -35,10 +44,11 @@ public class changeHealth : MonoBehaviour {
         {
             sprite.sprite = oneHp;
         }
+        //if no lives left, the game ends and goes to the end game screen
         else
         {
             sprite.sprite = dead;
-            Application.LoadLevel(4);
+            Application.LoadLevel(4);//end game screen
         }
     }
 }
--- Movement/Assets/Script/changeImage.cs
+++ Movement/Assets/Script/changeImage.cs
@@ -3,29 +3,52 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class changeImage : MonoBehaviour {
+    /// <summary>
+    /// variables that store the image reference and the current word
+    /// </summary>
     public RawImage img;
     public string currentWord;
+    public string word;
+    public float elapsedTime;
 
     // Use this for initialization
     void Start ()
     {
-        img = GetComponent<RawImage>();//
+        img = GetComponent<RawImage>();//refer to the Raw Image box on the field
         img.enabled = false; //hide image untill new word is given
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        ChangePicture();
-	}
+        word = Bubbles.GetWord();
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= 5)
+        {
+            img.enabled = false;
+        }
+        //if the current word is different from the previous the image is changing 
+        //
+        if (word != currentWord)
+        {
+            img.enabled = true;
+            ChangePicture();
+            for(int i =1; i>=5; i++)
+            {
+                elapsedTime += Time.deltaTime;
+            }
+        }  
+    }
 
     /// <summary>
     /// Method that changes the image for the current word
     /// </summary>
     public void ChangePicture()
     {
-        string word = Bubbles.GetWord();
-        GetComponent<RawImage>().texture = Resources.Load<Texture2D>(word);
+        currentWord = Bubbles.GetWord();
+        GetComponent<RawImage>().texture = Resources.Load<Texture2D>(currentWord);
         img.enabled = true;
+        elapsedTime = 0;
     }
+
 }
